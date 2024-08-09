67a9d50469664f9e11cdb44dd34d2c0ddc3d184a
EstebanValladares <131742625+EstebanValladares@users.noreply.github.com>
2024-08-09 13:49:12 -0600

    uso de json para enviar datos

--- operations_flask/basic_server.py
+++ operations_flask/basic_server.py
@@ -1,22 +1,26 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+app = Flask(__name__) # se crea la instancia de la clase Flask
+@app.route("/op", methods=['POST']) # se establece el endpoint y el método HTTP
 
-@app.route("/op", methods=['GET'])
-def operations():
-    number1 = float(request.args.get('number1', 0))
-    number2 = float(request.args.get('number1', 0))
+def operations():# se establece la función que se ejecutará al acceder al endpoint
+    data = request.get_json() #se establece la variable data que obtiene los datos enviados en la petición
+    number1 = float(data.get('number1')) #se obtiene el valor de la clave number1 y se convierte a float
+    number2 = float(data.get('number2')) #se obtiene el valor de la clave number2 y se convierte a float
 
-    suma = number1 + number2
-    resta = number1 - number2
-    multiplicacion = number1 * number2
-    division = number1 / number2 if number2 != 0 else "No se puede dividir por cero"
-
-    return jsonify({
-        "number1": number1,
+    #operaciones matemáticas
+    suma = number1 + number2;
+    resta = number1 - number2;
+    multiplicaicion = number1 * number2;
+    division = number1 / number2;
+    
+    return jsonify({ #se retorna un objeto JSON con los resultados de las operaciones
+        "number": number1,
         "number2": number2,
         "suma": suma,
         "resta": resta,
-        "multiplicacion": multiplicacion,
+        "multiplicacion": multiplicaicion,
         "division": division
     })
+if __name__ == '__main__': #se ejecuta el servidor en el puerto 5000
+    app.run(host= '0.0.0.0',port=5000,debug=False,use_reloader=False) 
